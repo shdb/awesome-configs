c418a64372d6c1e425771b0fb002c4263b832b76
shdb <shadowblade@gmx.ch>
2010-02-12 20:14:09 +0100

    shiny.keyboard: a module to toggle the keyboard layout

---
+++ shiny/keyboard/init.lua
@@ -0,0 +1,42 @@
+local awful   = require("awful")
+local naughty = require("naughty")
+
+local io = {
+    popen = io.popen,
+    close = io.close
+}
+local string = string
+module("shiny.keyboard")
+
+
+local function getkb()
+    local f = io.popen("setxkbmap -print")
+    for line in f:lines() do
+        local _, _, lo = string.find(line, "+(..)+")
+        if lo then
+            f:close()
+            return lo
+        end
+    end
+    f:close()
+end
+
+function toggle()
+    if not layout then
+        layout = getkb()
+    end
+    if layout == "ch" then
+        awful.util.spawn_with_shell("setxkbmap -layout us; "
+                .. "xmodmap " .. awful.util.getdir("config") .. "/../../.Xmodmap")
+        layout = "us"
+    else
+        awful.util.spawn_with_shell("setxkbmap -layout ch; "
+                .. "xmodmap " .. awful.util.getdir("config") .. "/../../.Xmodmap")
+        layout = "ch"
+    end
+    naughty.notify {
+        title = "keyboard layout",
+        text  = "current layout: " .. layout,
+        timeout = 2
+    }
+end
