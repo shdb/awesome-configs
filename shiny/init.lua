7509ec109454fcfd4609025dca6960d0798d21bf
shdb <shadowblade@gmx.ch>
2012-01-11 20:14:01 +0100

    shiny: handle empty or no arguments in fg

--- shiny/init.lua
+++ shiny/init.lua
@@ -19,6 +19,8 @@ function register(func, tout)
 end
 
 function fg(color, text)
+    color = color or "#ffffff"
+    text = text or ""
     return '<span color="' .. color .. '">' .. text .. '</span>'
 end
 
