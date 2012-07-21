2c9fa81c9a21d0b9ed552319b47bbd82d273b526
shdb <shadowblade@gmx.ch>
2012-07-21 19:37:33 +0200

    rc: remove height from wibox

--- rc.lua
+++ rc.lua
@@ -239,7 +239,7 @@ for s = 1, screen.count() do
     mytaglist[s] = awful.widget.taglist(s, awful.widget.taglist.label.all, mytaglist.buttons)
 
     -- Create the wibox
-    mywibox[s] = awful.wibox({ position = "top", screen = s, height = 15 })
+    mywibox[s] = awful.wibox({ position = "top", screen = s})
     -- Add widgets to the wibox - order matters
     mywibox[s].widgets = {
         {
