69d817fc19d3d8bda3ea3ddda62f4639efdc13cc
shdb <shadowblade@gmx.ch>
2012-12-09 08:44:20 +0100

    shiny.lock: use i3lock

--- shiny/lock/init.lua
+++ shiny/lock/init.lua
@@ -3,5 +3,5 @@ local awful   = require("awful")
 module("shiny.lock")
 
 function lock(c)
-    awful.util.spawn_with_shell("killall unclutter; xtrlock; unclutter -grab -idle 1 &")
+    awful.util.spawn_with_shell("killall unclutter; i3lock -c 000000 -d; unclutter -grab -idle 1 &")
 end
